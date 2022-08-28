61a26d75ef43bc2b1b7aef600b5d51f7bd754469
anaard <anareginadelazzari@gmail.com>
2022-08-28 09:59:03 -0300

    apresentação membro 3 completa

--- membro3.py
+++ membro3.py
@@ -1,2 +1,2 @@
 def hello():
-	return """Olá, eu sou Ana Regina Delazzari, tenho 18 anos e os meus hobbies são ler, programar, escutar música e ver filmes. """ #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return """Olá, eu sou Ana Regina Delazzari, sou de Caibi, tenho 18 anos e os meus hobbies são ler, programar, escutar música e ver filmes. \nAntes de entrar no curso, eu tive pouca experiência com programação, apenas o básico de python. \nPara esta disciplina eu espero aumentar o meu conhecimento em programação orientada a objetos.""" 
